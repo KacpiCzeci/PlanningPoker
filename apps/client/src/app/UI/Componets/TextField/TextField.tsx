e5072224406f4263ea1597dd3781884d1dd9986f
Maciej Ziobrowski <77328018+Ziob3r@users.noreply.github.com>
2022-04-18 23:01:34 +0200

    Average TextField & Website Layout
    Adding fields for the average points value from voting, page layout (prototype) and changing the application icon.

--- apps/client/src/app/UI/Componets/TextField/TextField.tsx
+++ apps/client/src/app/UI/Componets/TextField/TextField.tsx
@@ -9,28 +9,44 @@ interface TextFieldProps {
     autoFocus?: boolean;
     name?: string;
     type?: 'email' | 'password' | 'text';
+    destiny ?: 'name-of-vote'| 'average';
 
 }
 function TextField( props : TextFieldProps) {
-
-
-    const changeHandler = (e: { target: { value: string; }; }) => {
+    function checkDestiny (){
+        if(props.destiny==="name-of-vote")
+        {return 1;}
+        if(props.destiny==="average")
+        {return 2;}else{return 0;}
+    };
+    function changeHandler(e: { target: { value: string; }; }) {
         props.onChange(e.target.value);
     }
 
+    if(checkDestiny()===1){
+        return (
+            <form> 
+                <p>
+                    <label className="text-field-label-leftcornel" htmlFor="input">{props.placeholder}</label>
+                </p>
+                <input className="text-field-input-standard"
+                    id="input"
+                    onChange={changeHandler} />
+            </form>
+    )
+    }
+    if(checkDestiny()===2){
+        return (
+            <form> 
+                <label className ="text-field-label-left" htmlFor="input">{props.placeholder}: </label>
+                <input className="text-field-input-result" disabled
+                    id="input"
+                    value={props.value}
+                    onChange={changeHandler} />
+            </form>
+    )
+    }
 
-    return (
-        <form className="test">
-            <p>
-            <label className="text-field-label-v" htmlFor="input">{props.placeholder}</label>
-            </p>
-
-            <input className="text-field-input"
-                id="input"
-                onChange={changeHandler}
-            />
-            
-        </form>
-    );
+    return(<p>Here shoud be a textfield <p className="error">{props.name}</p></p>)
 }
 export default TextField;
--- apps/client/src/app/UI/GamePage/GamePage.tsx
+++ apps/client/src/app/UI/GamePage/GamePage.tsx
@@ -4,29 +4,52 @@ import "./GamePage.scss"
 
 export default function GamePage(){
     const [val, setVal] = useState("");
+    const [val2, setVal2] = useState("2");
     return(
         <div className="GamePage-container">
+            <div className="GamePage-header">
+                <h1>Planning Poker</h1>
+            </div>
             <div className="GamePage-userinfobar">
-                <div className="GamePage-gamegsers">
+                <div className="GamePage-gameusers">
+                    Game Users
                 </div>
                 <div className="GamePage-username">
+                    Game user nama
                 </div>
             </div>
             <div className="GamePage-gameinfo">
                 <div className="GamePage-gamename">
                     <TextField  placeholder="Name of vote" 
                     value={val}
-                    onChange={setVal} />   
+                    onChange={setVal}
+                    name="name of vote"
+                    destiny="name-of-vote"/>   
                 </div>
                 <div className="GamePage-vote">
                 </div>
-                <div className="GamePage-votingresults">
+                <div className="GamePage-voting-results">
+                <TextField  placeholder="Average" 
+                    value={val}
+                    onChange={setVal2}
+                    name="avr1"
+                    destiny="average"/> 
+
+                     <TextField  placeholder="Average 2" 
+                    value={val}
+                    onChange={setVal2}
+                    name="avr2"
+                    destiny="average"/>   
                 </div>
+
+                
             </div>
             <div className="GamePage-voteinfo">
                 <div className="GamePage-voteoptions">
+                    Karty
                 </div>
                 <div className="GamePage-newvote">
+                    NewVote
                 </div>
             </div>
         </div>
