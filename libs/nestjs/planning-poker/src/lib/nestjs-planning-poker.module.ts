6573fe403a84bbe789f3c9cd09097c8257e17718
Bartłomiej Mumot <bartlomiej.mumot@gmail.com>
2022-04-23 11:30:01 +0200

    BREAKING CHANGE: change vote endpoints

--- libs/nestjs/planning-poker/src/lib/nestjs-planning-poker.module.ts
+++ libs/nestjs/planning-poker/src/lib/nestjs-planning-poker.module.ts
@@ -1,10 +1,9 @@
 import { DatastoreService } from './../datastore/datastore.service';
 import { Module } from '@nestjs/common';
-import { BoardController } from '../board/board.controller';
 import { VotingController } from '../voting/voting.controller';
 
 @Module({
-  controllers: [VotingController, BoardController],
+  controllers: [VotingController],
   providers: [DatastoreService],
   exports: [],
 })
