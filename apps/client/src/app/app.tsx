4ed30c9c2667c03b2a84df5d88b0df68b3fafd4a
KacpiCzeci <kapixxx666@wp.pl>
2022-06-13 17:41:28 +0200

    To work on PC

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,11 +1,12 @@
 import { GlobalStateProvider, useGlobalState } from './GlobalStateProvider';
-import LoginPage from './UI/Pages/LoginPage/LoginPage';
+import LoginPage from './UI/Pages/LoginPage1/LoginPage';
 import { useState } from 'react';
 import NotFoundPage from './UI/Pages/NotFoundPage/NotFoundPage';
 import CreateGamePage from './UI/Pages/CreateGamePage/CreteGamePage';
 import CheckPage from './UI/Pages/CheckPage/CheckPage';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GamePage from './UI/Pages/GamePage/GamePage';
+import ReqAuthRoute from './UI/Pages/ReqAuthPage/ReqAuthPage';
 
 export function App() {
   return (
@@ -16,6 +17,9 @@ export function App() {
           {/* <Route path="/" element={<CreateGamePage/>}/> */}
           <Route path="/" element={<GamePage/>}/>
           <Route path="/:id" element={<CheckPage/>}/>
+          <Route path="/new" element={<ReqAuthRoute element={<CreateGamePage/>}/>}/>
+          <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/:id" element={<ReqAuthRoute element={<CheckPage/>}/>}/>
           <Route path="/*" element={<NotFoundPage/>}/>
         </Routes>
       </BrowserRouter>
