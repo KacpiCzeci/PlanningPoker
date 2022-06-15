ee1a4b0f1f6b88a19b9fa7f619ef1847f97ad5ea
Veltarrox <jasiuu55@wp.pl>
2022-06-15 12:17:07 +0200

    Update UserList.tsx

--- apps/client/src/app/UI/Componets/UserList/UserList.tsx
+++ apps/client/src/app/UI/Componets/UserList/UserList.tsx
@@ -12,19 +12,18 @@ export interface UserListProps {
 export function UserList(props: UserListProps) {
   const { state, setState } = useGlobalState();
   
-  const usersValues = [{"name" : "User1" , "card" : undefined},{"name" : "User2" , "card" : 2},{"name" : "User3" , "card" : 3}];
   const userss = props.users.map(value => {
-    if(value.player !== state.userName){
+    // if(value.player !== state.userName){
       return <li key={value.player} className="UserList-item" ><User key={value.player} value={value.score??undefined} userName={value.player} players={props.users} /></li>
-    } 
-    else {
-      return null;
-    } 
+    // } 
+    // else {
+    //   return null;
+    // } 
   })
   return (
     <div className='UserList-container'>
       <ul className='UserList-subcontainer'>
-        <li key={state.userName} className="UserList-item" ><User key={state.userName} value={state.cardPicked} userName={state.userName} players={props.users}/></li>
+        {/* <li key={state.userName} className="UserList-item" ><User key={state.userName} value={state.cardPicked} userName={state.userName} players={props.users}/></li> */}
         {userss}
       </ul>
     </div>
